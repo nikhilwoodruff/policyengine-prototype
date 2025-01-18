3a7aa807f14da962b0815fd683ff026ef0b6225c
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-01-18 11:42:25 +1000

    Move API into app folder

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { config } from 'dotenv';
+
+config({ path: '../.env' });
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    LOCAL: process.env.LOCAL,
+    SUPABASE_URL: process.env.LOCAL ?
+      process.env.SUPABASE_URL :
+      process.env.SUPABASE_LOCAL_URL,
+    SUPABASE_KEY: process.env.LOCAL ?
+      process.env.SUPABASE_KEY :
+      process.env.SUPABASE_LOCAL_KEY,
+  }
 };
 
 export default nextConfig;
