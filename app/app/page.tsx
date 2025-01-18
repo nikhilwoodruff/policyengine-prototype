e2d5be9a05d8c38fa9221564b4f98b1f7483ac4f
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-01-18 11:10:52 +1000

    Update app

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -33,6 +33,7 @@ async function submitJob(jobDetails: string) {
 export default function Home() {
   const [jobDetails, setJobDetails] = useState<string>("");
   const [jobId, setJobId] = useState<string>("");
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   const [jobData, setJobData] = useState<any>(null);
   return (
     <div className="h-screen flex justify-center items-center">
@@ -41,11 +42,10 @@ export default function Home() {
         <p className="text-gray-600">Create a simulation job here.</p>
         <Textarea className="w-full h-1/2" placeholder="Enter job details here" value={jobDetails} onChange={(e) => setJobDetails(e.target.value)} />
         <Button className="w-full" onClick={() => submitJob(jobDetails).then(value => {
-          console.log(value);
             setJobId(value);
             supabase.channel(`job_${value}`).on(
               "postgres_changes", 
-              { schema: "public", table: "job", event: "*" },
+              { schema: "public", table: "job", event: "*", filter: "id=eq." + value },
               (payload) => {
                 console.log("Change received!", payload);
                 setJobData(payload);
@@ -57,6 +57,7 @@ export default function Home() {
       <Card className="h-3/4 w-3/4 p-4 m-4">
         <h1 className="text-2xl font-bold">Job results</h1>
         <p className="text-gray-600">Results will appear here.</p>
+        <h4>Job {jobId}</h4>
         <p>{jobData?.new?.result ? JSON.stringify(jobData.new.result) : ""}</p>
       </Card>
     </div>
